ee2f0a5c64bd6f7cebb2d6dcc2bc4a232f768501
Rolando Cosme <rolando.cosme@gmail.com>
2018-05-17 22:07:16 -0400

    added favicon and removed mailgun code

--- server.js
+++ server.js
@@ -18,17 +18,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, function(){
   console.log("Listening on " + PORT);
 });
-
-
-
-
-// var mg = new Mailgun('key-8f469d15520273679a514faa0b840318');
-
-// mg.sendText('rcosmedeveloper@gmail.com', ['Recipient 1 <rolando.cosme@gmail.com>'],
-//   'This is the subject - mailgun',
-//   'This is the text - mailgun',
-//   'noreply@example.com', {},
-//   function(err) {
-//     if (err) console.log('Oh noes: ' + err);
-//     else     console.log('Success');
-// });
