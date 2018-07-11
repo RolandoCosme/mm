c51b7662c4c9bb1211054fa6853af85367390e6e
Rolando Cosme <rolando.cosme@gmail.com>
2018-07-11 16:28:38 -0400

    still on the same issue with the paths but this time it actually works

--- public/js/app.js
+++ public/js/app.js
@@ -8,35 +8,35 @@ angular.module('Minds', [
         templateUrl: '/public/partials/partial-home.html'
       })
       .when('/gallery',{
-          templateUrl: 'partials/partial-gallery.html',
+          templateUrl: '/public/partials/partial-gallery.html',
       })
       .when('/practical-life',{
-          templateUrl: 'partials/partial-practical.html',
+          templateUrl: '/public/partials/partial-practical.html',
       })
       .when('/sensorial',{
-          templateUrl: 'partials/partial-sensorial.html',
+          templateUrl: '/public/partials/partial-sensorial.html',
       })
       .when('/mathematics',{
-          templateUrl: 'partials/partial-mathematics.html',
+          templateUrl: '/public/partials/partial-mathematics.html',
       })
       .when('/language',{
-          templateUrl: 'partials/partial-language.html',
+          templateUrl: '/public/partials/partial-language.html',
       })
       .when('/science',{
-          templateUrl: 'partials/partial-science.html',
+          templateUrl: '/public/partials/partial-science.html',
       })
       .when('/geography',{
-          templateUrl: 'partials/partial-geography.html',
+          templateUrl: '/public/partials/partial-geography.html',
       })
       .when('/art',{
-          templateUrl: 'partials/partial-art.html',
+          templateUrl: '/public/partials/partial-art.html',
       })
       .when('/enrollment',{
-          templateUrl: 'partials/partial-enrollment.html',
+          templateUrl: '/public/partials/partial-enrollment.html',
           controller: 'submitController.js'
       })
       .when('/contact',{
-          templateUrl: 'partials/partial-contact.html',
+          templateUrl: '/public/partials/partial-contact.html',
           // controller: 'mailController.js'
       })
       .otherwise({
